8db2c80cff9c8d849f340d5f4af24d2fd4a7a949
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-08-02 12:01:06 -0500

    Update chatController.js

---
+++ src/controllers/chatController.js
@@ -0,0 +1,32 @@
+const ai = require("../config/gemini");
+
+const chat = async (req, res) => {
+
+    try {
+
+        const { message } = req.body;
+
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: message
+        });
+
+        res.json({
+            success: true,
+            reply: response.text
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            error: "Error interno"
+        });
+
+    }
+
+};
+
+module.exports = { chat };
