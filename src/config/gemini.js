62a3568b47aae49fc77ed0415af564ac2192952c
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-08-02 13:19:50 -0500

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,5 +1,8 @@
 const { GoogleGenAI } = require("@google/genai");
 
+console.log("API Key existe:", !!process.env.GEMINI_API_KEY);
+console.log("Inicio de la key:", process.env.GEMINI_API_KEY?.substring(0, 10));
+
 const ai = new GoogleGenAI({
     apiKey: process.env.GEMINI_API_KEY
 });
